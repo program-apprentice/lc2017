7330d8edf40914962e1e59563811e43e08d3d98d
Rui Jiang <rui.jiang.dev@gmail.com>
2017-10-08 19:11:11 -0700

    038 strStr Add brute force solution

--- java/lc-app/src/main/java/com/programapprentice/app/easy/StrStr038.java
+++ java/lc-app/src/main/java/com/programapprentice/app/easy/StrStr038.java
@@ -20,69 +20,37 @@ public class StrStr038 {
      Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
      * */
 
-
-    public int[] getDiffTable(String needle) {
-        int n = needle.length();
-        int pos = 1;
-        int cnd = 0;
-        int[] table = new int[n];
-        table[0] = -1;
-        while(pos < n) {
-            if (needle.charAt(pos) == needle.charAt(cnd)) {
-                table[pos] = table[cnd];
-                pos += 1;
-                cnd += 1;
-            } else {
-                table[pos] = cnd;
-                cnd = table[cnd];
-                while (cnd >= 0 && needle.charAt(pos) != needle.charAt(cnd)) {
-                    cnd = table[cnd];
-                }
-                pos += 1;
-                cnd += 1;
-            }
-        }
-        if (pos < n) {
-            table[pos] = cnd;
-        }
-        return table;
-    }
-
-    /**
-     * KMP algorithm in WIKI page
-     * https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
-     * */
     public int strStr(String haystack, String needle) {
         if (needle == null || haystack == null) {
             return -1;
         }
-        if (needle.equals("") && haystack.equals("")) {
-            return 0;
-        } else if (needle.equals("") || haystack.equals("")) {
+        if (needle.length() > haystack.length()) {
             return -1;
         }
-
-        int m = 0;
+        if (needle.equals("") && haystack.equals("")) {
+            return 0;
+        } else if (needle.equals("")) {
+            return 0;
+        }
         int i = 0;
-        int stackLength = haystack.length();
-        int needleLength = needle.length();
-        int[] table = getDiffTable(needle);
-
-        while (m + i < stackLength) {
-            if(needle.charAt(i) == haystack.charAt(m+i)) {
-                i +=1;
-                if (i == needleLength) { // find it.
-                    return m;
-                }
-                m += i - table[i];
-                i = table[i];
-            } else {
-                if (table[i] > -1) {
-                    m += i - table[i];
-                    i = table[i];
+        int j = 0;
+        int startIdx = 0;
+        int lengthMatched = 0;
+        while(i < haystack.length() && j < needle.length()) {
+            while(j < needle.length() && i < haystack.length()) {
+                if (needle.charAt(j) == haystack.charAt(i)) {
+                    i++;
+                    j++;
+                    lengthMatched ++;
+                    if (lengthMatched == needle.length()) {
+                        return startIdx;
+                    }
                 } else {
-                    m = m + i + 1;
-                    i = 0;
+                    lengthMatched = 0;
+                    i = startIdx + 1;
+                    startIdx += 1;
+                    j = 0;
+                    break;
                 }
             }
         }
