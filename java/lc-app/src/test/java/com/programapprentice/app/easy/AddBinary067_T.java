ddc83aa238bff11d9fe236513c9b96a4fe2dcecc
Rui Jiang <rui.jiang.dev@gmail.com>
2017-10-09 19:30:17 -0700

    [067] Add Binary

---
+++ java/lc-app/src/test/java/com/programapprentice/app/easy/AddBinary067_T.java
@@ -0,0 +1,23 @@
+package com.programapprentice.app.easy;
+
+import org.junit.Test;
+
+public class AddBinary067_T {
+    AddBinary067 solution = new AddBinary067();
+
+    @Test
+    public void test1() {
+        String s = "0";
+        String t = "0";
+        String output = solution.addBinary(s, t);
+        System.out.println(output);
+    }
+
+    @Test
+    public void test2() {
+        String s = "11";
+        String t = "1";
+        String output = solution.addBinary(s, t);
+        System.out.println(output);
+    }
+}
