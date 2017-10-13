bc757d6442082764e400d59fd919ccd187ba53fb
Rui Jiang <rui.jiang.dev@gmail.com>
2017-10-12 21:43:48 -0700

    easy [172] Factorial Trailing Zeroes

---
+++ java/lc-app/src/test/java/com/programapprentice/app/easy/FactorialTrailingZeroes_172_T.java
@@ -0,0 +1,15 @@
+package com.programapprentice.app.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FactorialTrailingZeroes_172_T {
+    FactorialTrailingZeroes_172 solution = new FactorialTrailingZeroes_172();
+
+    @Test
+    public void test1() {
+        int result = solution.trailingZeroes(1808548329);
+        int expected = 452137076;
+        Assert.assertEquals(result, expected);
+    }
+}
