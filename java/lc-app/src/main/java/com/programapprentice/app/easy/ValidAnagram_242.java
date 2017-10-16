0c111be6b4193c559cb5f5c0da3c63d53ad6eb9e
Program Apprentice <rui.jiang.tech@gmail.com>
2017-10-15 21:20:22 -0700

    easy [242] Valid Anagram (one submission)

---
+++ java/lc-app/src/main/java/com/programapprentice/app/easy/ValidAnagram_242.java
@@ -0,0 +1,42 @@
+package com.programapprentice.app.easy;
+
+public class ValidAnagram_242 {
+    /**
+     [242] Valid Anagram
+
+     https://leetcode.com/problems/valid-anagram
+
+     Given two strings s and t, write a function to determine if t is an anagram of s.
+
+     For example,
+     s = "anagram", t = "nagaram", return true.
+     s = "rat", t = "car", return false.
+
+     Note:
+     You may assume the string contains only lowercase alphabets.
+
+     Follow up:
+     What if the inputs contain unicode characters? How would you adapt your solution to such case?
+    * */
+
+    public int[] getDict(String s) {
+        int[] sdict = new int[26];
+        for(char c : s.toCharArray()) {
+            if (c != ' ') {
+                sdict[c - 'a'] ++;
+            }
+        }
+        return sdict;
+    }
+
+    public boolean isAnagram(String s, String t) {
+        int[] sdict = getDict(s);
+        int[] tdict = getDict(t);
+        for(int i = 0; i < 26; i++) {
+            if (sdict[i] != tdict[i]) {
+                return  false;
+            }
+        }
+        return true;
+    }
+}
