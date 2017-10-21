f8a9928a6044c95e6e7398f27d51e805230050f3
Program Apprentice <rui.jiang.tech@gmail.com>
2017-10-20 22:57:46 -0700

    medium [275] H-Index II

---
+++ java/lc-app/src/main/java/com/programapprentice/app/medium/HIndexII_275.java
@@ -0,0 +1,37 @@
+package com.programapprentice.app.medium;
+
+import java.util.Arrays;
+
+public class HIndexII_275 {
+    /**
+     [275] H-Index II
+
+     https://leetcode.com/problems/h-index-ii
+
+     Follow up for H-Index: What if the citations array is sorted in ascending order? Could you optimize your algorithm?
+     * */
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+        if (citations.length == 1) {
+            if (citations[0] == 0) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+
+
+        int n = citations.length;
+        for(int i = 0; i < n; i++) {
+            int numOfPapers = n - i;
+            if (citations[i] > numOfPapers) {
+                return numOfPapers;
+            }
+        }
+
+        return 0;
+    }
+
+}
