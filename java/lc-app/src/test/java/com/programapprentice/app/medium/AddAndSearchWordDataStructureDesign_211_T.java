1bb68a6329b9b593df108849ac3b32fa59e136cf
Program Apprentice <rui.jiang.tech@gmail.com>
2017-10-21 23:04:27 -0700

    medium [211] Add and Search Word Data Structure Design

---
+++ java/lc-app/src/test/java/com/programapprentice/app/medium/AddAndSearchWordDataStructureDesign_211_T.java
@@ -0,0 +1,20 @@
+package com.programapprentice.app.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AddAndSearchWordDataStructureDesign_211_T {
+    AddAndSearchWordDataStructureDesign_211 solution = new AddAndSearchWordDataStructureDesign_211();
+
+    @Test
+    public void test1() {
+        AddAndSearchWordDataStructureDesign_211.WordDictionary dict = new AddAndSearchWordDataStructureDesign_211.WordDictionary();
+        dict.addWord("bad");
+        dict.addWord("dad");
+        dict.addWord("mad");
+        Assert.assertFalse(dict.search("pad"));
+        Assert.assertTrue(dict.search("mad"));
+        Assert.assertTrue(dict.search(".ad"));
+        Assert.assertTrue(dict.search("b.."));
+    }
+}
