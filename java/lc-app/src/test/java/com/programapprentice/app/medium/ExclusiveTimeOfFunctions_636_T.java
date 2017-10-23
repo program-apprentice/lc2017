5670eccd4c69f5757d159b8b335b80495a4b38f2
Program Apprentice <rui.jiang.tech@gmail.com>
2017-10-23 10:17:22 -0700

    * medium [636] Exclusive Time Off Functions || consider cur start/end and pre start/end

---
+++ java/lc-app/src/test/java/com/programapprentice/app/medium/ExclusiveTimeOfFunctions_636_T.java
@@ -0,0 +1,37 @@
+package com.programapprentice.app.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExclusiveTimeOfFunctions_636_T {
+    ExclusiveTimeOfFunctions_636 solution = new ExclusiveTimeOfFunctions_636();
+    @Test
+    public void test1() {
+        List<String> input = new ArrayList<String>();
+        input.add("0:start:0");
+        input.add("0:start:2");
+        input.add("0:end:5");
+        input.add("0:start:6");
+        input.add("0:end:6");
+        input.add("0:end:7");
+        int[] result = solution.exclusiveTime(1, input);
+        Assert.assertEquals(8, result[0]);
+    }
+
+    @Test
+    public void test2() {
+        List<String> input = new ArrayList<String>();
+        input.add("0:start:0");
+        input.add("0:start:2");
+        input.add("0:end:5");
+        input.add("1:start:6");
+        input.add("1:end:6");
+        input.add("0:end:7");
+        int[] result = solution.exclusiveTime(2, input);
+        Assert.assertEquals(7, result[0]);
+        Assert.assertEquals(1, result[1]);
+    }
+}
