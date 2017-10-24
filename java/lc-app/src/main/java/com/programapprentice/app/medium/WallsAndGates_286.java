1f7f9356647ecee767177505a015325a2badee6b
Program Apprentice <rui.jiang.tech@gmail.com>
2017-10-23 23:19:37 -0700

    * medium [286] Walls and Gates || Doing DFS/BFS from gate instead of room to optimize time

--- java/lc-app/src/main/java/com/programapprentice/app/medium/WallsAndGates_286.java
+++ java/lc-app/src/main/java/com/programapprentice/app/medium/WallsAndGates_286.java
@@ -27,66 +27,17 @@ public class WallsAndGates_286 {
      0  -1   3   4
      * */
 
-    class Point{
-        int row;
-        int col;
-        Point(int x, int y) {
-            this.row = x;
-            this.col = y;
+    // The key is do it from door, so you can cut branch quickly
+    public void dfs(int[][] rooms, int i, int j, int height, int width, int depth) {
+        if (i < 0 || i >= height || j < 0 || j >= width || rooms[i][j] < depth || rooms[i][j] == -1) {
+            return;
         }
+        rooms[i][j] = depth;
+        dfs(rooms, i-1, j, height, width, depth+1);
+        dfs(rooms, i+1, j, height, width, depth+1);
+        dfs(rooms, i, j+1, height, width, depth+1);
+        dfs(rooms, i, j+1, height, width, depth+1);
     }
-
-    private boolean isRoomOrGate(int row, int col, int[][] rooms) {
-        return rooms[row][col] != -1;
-    }
-
-    private void fillOneRoom(int row, int col, int[][] rooms) {
-        if (rooms[row][col] == 0 || rooms[row][col] == -1) {
-            return;// wall or gate, no need to change
-        }
-        Queue<Point> queue = new LinkedList<Point>();
-        Queue<Integer> lengths = new LinkedList<Integer>();
-        queue.add(new Point(row, col));
-        lengths.add(0);
-        int height = rooms.length;
-        int width = rooms[0].length;
-        boolean[][] rec = new boolean[height][width];
-        while(!queue.isEmpty()) {
-            Point cur = queue.remove();
-            int length = lengths.remove();
-            rec[cur.row][cur.col] = true;
-            if (rooms[cur.row][cur.col] == 0) {
-                // is gate
-                rooms[row][col] = length;
-                return;
-            }
-
-            // left
-            if (cur.col-1 >= 0 && isRoomOrGate(cur.row, cur.col-1, rooms) && !rec[cur.row][cur.col-1]) {
-                queue.add(new Point(cur.row, cur.col-1));
-                lengths.add(length+1);
-            }
-
-            // right
-            if (cur.col+1 < width && isRoomOrGate(cur.row, cur.col+1, rooms)  && !rec[cur.row][cur.col+1]) {
-                queue.add(new Point(cur.row, cur.col+1));
-                lengths.add(length+1);
-            }
-
-            // down
-            if (cur.row+1 < height && isRoomOrGate(cur.row+1, cur.col, rooms)  && !rec[cur.row+1][cur.col]) {
-                queue.add(new Point(cur.row+1, cur.col));
-                lengths.add(length+1);
-            }
-
-            // up
-            if (cur.row-1 >= 0 && isRoomOrGate(cur.row-1, cur.col, rooms) && !rec[cur.row-1][cur.col]) {
-                queue.add(new Point(cur.row-1, cur.col));
-                lengths.add(length+1);
-            }
-        }
-    }
-
 
     public void wallsAndGates(int[][] rooms) {
         if (rooms == null || rooms.length == 0 || rooms[0] == null || rooms[0].length == 0) {
@@ -96,8 +47,11 @@ public class WallsAndGates_286 {
         int width = rooms[0].length;
         for(int i = 0; i < height; i++) {
             for(int j = 0; j < width; j++) {
-                fillOneRoom(i, j, rooms);
+                if (rooms[i][j] == 0) {
+                    dfs(rooms, i, j, height, width, 0);
+                }
             }
         }
     }
+
 }
