30afec32b8b3253d392bc8d01299f4b00c3b916a
Rui Jiang <rui.jiang.tech@gmail.com>
2017-12-28 23:39:28 -0800

    medium [544] Output Contest Matches

---
+++ java/lc-app/src/test/java/com/programapprentice/app/google/medium/OutputContestMatches_544_T.java
@@ -0,0 +1,13 @@
+package com.programapprentice.app.google.medium;
+
+import org.junit.Test;
+
+public class OutputContestMatches_544_T {
+    OutputContestMatches_544 solution = new OutputContestMatches_544();
+
+    @Test
+    public void test1() {
+        int n = 1024;
+        System.out.println(Math.log(n)/ Math.log(2));
+    }
+}
