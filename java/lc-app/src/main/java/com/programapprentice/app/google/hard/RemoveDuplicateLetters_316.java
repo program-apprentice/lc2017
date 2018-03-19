a2c552c7edf146347621c69aec32f23db2cc4988
Rui Jiang <rui.jiang.tech@gmail.com>
2018-03-19 10:48:34 -0700

    WIP hard [316] Remove Duplicate Letters

---
+++ java/lc-app/src/main/java/com/programapprentice/app/google/hard/RemoveDuplicateLetters_316.java
@@ -0,0 +1,22 @@
+package com.programapprentice.app.google.hard;
+
+public class RemoveDuplicateLetters_316 {
+    /**
+     https://leetcode.com/problems/remove-duplicate-letters/description/
+
+     Given a string which contains only lowercase letters, remove duplicate letters so that every letter appear once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+
+     Example:
+     Given "bcabc"
+     Return "abc"
+
+     Given "cbacdcbc"
+     Return "acdb"
+
+     Solution:
+     https://leetcode.com/problems/remove-duplicate-letters/discuss/76766/Easy-to-understand-iterative-Java-solution
+     * */
+    public String removeDuplicateLetters(String s) {
+
+    }
+}
